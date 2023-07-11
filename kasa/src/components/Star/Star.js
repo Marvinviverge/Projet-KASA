130fb566ab15c22e7e1c26d11743c232af709479
Marvin Viverge <marvin.viverge@gmail.com>
2023-07-11 09:41:07 +0200

    optimisation component star

--- kasa/src/components/Star/Star.js
+++ kasa/src/components/Star/Star.js
@@ -7,11 +7,20 @@ import Starinactive from '@/Assets/images/star-inactive.png'
 const Star = (rating) => {
     return (
         <div className='Rate'>
-            <img src={rating.rating > 0 ? Staractive : Starinactive} alt='Location Star Rate' />
-            <img src={rating.rating > 1 ? Staractive : Starinactive} alt='Location Star Rate' />
-            <img src={rating.rating > 2 ? Staractive : Starinactive} alt='Location Star Rate' />
-            <img src={rating.rating > 3 ? Staractive : Starinactive} alt='Location Star Rate' />
-            <img src={rating.rating > 4 ? Staractive : Starinactive} alt='Location Star Rate' />
+            {
+                [...Array(5)].map((_, index) => {
+                    if (index + 1 > rating.rating) {
+                        return (
+                            <img src={Starinactive} alt="rating star" key={index} className="starImg" />
+                        )
+                    }
+                    else {
+                        return (
+                            <img src={Staractive} alt="rating star" key={index} className="starImg" />
+                        )
+                    }
+                })
+            }
         </div>
     )
 
